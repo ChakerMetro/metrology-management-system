fd88f74a98f149810e9455d46dda91ac0be5d37a
ChakerMetro <amarchi.ahmedchaker@gmail.com>
2026-05-31 13:31:42 +0100

    Add main menu

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Metrology Management System")
+def main():
+    while True:
+        print("\n=== Metrology Management System ===")
+        print("1. Add Instrument")
+        print("2. View Instruments")
+        print("3. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            print("Add Instrument selected")
+
+        elif choice == "2":
+            print("View Instruments selected")
+
+        elif choice == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice")
+
+
+main()
