b13b89d3e634ca8073ffeef63872fddc86420815
ChakerMetro <amarchi.ahmedchaker@gmail.com>
2026-06-01 11:37:14 +0100

    Add instrument management

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def main():
+    instruments = []
     while True:
         print("\n=== Metrology Management System ===")
         print("1. Add Instrument")
@@ -8,10 +9,29 @@ def main():
         choice = input("Choose an option: ")
 
         if choice == "1":
-            print("Add Instrument selected")
+            name = input("Instrument name: ")
+            serial = input("Serial number: ")
+
+            instrument = {
+            "name": name,
+            "serial": serial
+            }
+
+            instruments.append(instrument)
+
+            print("Instrument added successfully!")
 
         elif choice == "2":
-            print("View Instruments selected")
+            if len(instrument) == 0:
+               print("No instruments found.")
+            else:
+               print("\n--- Instruments ---")
+
+               for instrument in instruments:
+                   print(
+                       f"Name: {instrument['name']} | "
+                       f"Serial: {instrument['serial']}" 
+                   )   
 
         elif choice == "3":
             print("Goodbye!")
